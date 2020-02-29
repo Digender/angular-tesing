dde8b3b36e94ec16d611e90c862420b5062500a6
Digender <mdigender@gmail.com>
2020-02-29 11:13:17 +0530

    Mocking child component, Dealing with Lists of element

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,15 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from '../hero.service';
 import { of } from 'rxjs';
+import { HeroComponent } from '../hero/hero.component';
+import { Component, Input } from '@angular/core';
 
 
 describe('Heroes Component Shallow Test', () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let heroServiceMock;
     let HEROES;
+
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>',
+    })
+    class FakeHeroComponent {
+        @Input() hero: HeroComponent;
+    }
+
     beforeEach(() => {
         HEROES = [
             { id: 1, name: 'SpiderDude', strength: 8 },
@@ -18,9 +28,8 @@ describe('Heroes Component Shallow Test', () => {
         ]
         heroServiceMock = jasmine.createSpyObj('HeroService', ['getHeroes', 'addHeroes', 'deleteHeroes']);
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [HeroesComponent, FakeHeroComponent],
             providers: [{ provide: HeroService, useValue: heroServiceMock }],
-            schemas: [NO_ERRORS_SCHEMA],
         });
         fixture = TestBed.createComponent(HeroesComponent);
     });
@@ -28,5 +37,12 @@ describe('Heroes Component Shallow Test', () => {
         heroServiceMock.getHeroes.and.returnValue(of(HEROES));
         fixture.detectChanges();
         expect(fixture.componentInstance.heroes.length).toBe(3);
+    });
+
+    it('should create 1 li for each hero', () => {
+        heroServiceMock.getHeroes.and.returnValue(of(HEROES));
+        fixture.detectChanges();
+
+        expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
     });
 });
